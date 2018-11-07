05ca5938608c9394dbb48da1c967b5ada075a10b
hotamisli <salihahotamisli@gmail.com>
2018-11-07 13:27:18 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function selected() {
+    var continent = document.getElementsByName('continent')
+
+    for (var i = 0; i < length.continent; i++) {
+        if (continent[i].checked) {
+
+            if (continent[i].value != '7') {
+                alert('it is wrong')
+            } else {
+                alert("Answer is correct!");
+            }
+            break;
+        }
+        // var option = document.querySelector('input[name="continent"]:checked').value
+
+    }
+
+
+}
