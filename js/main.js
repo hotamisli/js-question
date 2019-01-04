c8bfbf342fac965f5ce0f6beac6dcb72cbde545d
hotamisli <salihahotamisli@gmail.com>
2019-01-04 22:32:35 +0000

    fixed passing variable to url

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
+const button = document.getElementById("submitAnswer");
+
+//event listener on click
+button.addEventListener('click', quizSubmit, false)
+
 function quizSubmit() {
     var total = 5;
     var score = 0;
     var wrong = 0;
     var empty = 0;
-    var answers = ["b", "a", "b", "a", "c"]
+    var answers = ["b", "a", "b", "a", "c"];
 
     //Get selected elements from the form 
-    var question1 = document.forms["quiz"]["question1"].value;
-    var question2 = document.forms["quiz"]["question2"].value;
-    var question3 = document.forms["quiz"]["question3"].value;
-    var question4 = document.forms["quiz"]["question4"].value;
-    var question5 = document.forms["quiz"]["question5"].value;
-
+    var question1 = document.querySelector('input[name="question1"]:checked')
+    var question2 = document.querySelector('input[name="question2"]:checked')
+    var question3 = document.querySelector('input[name="question3"]:checked')
+    var question4 = document.querySelector('input[name="question4"]:checked')
+    var question5 = document.querySelector('input[name="question5"]:checked')
 
     // check submissions
     for (var i = 1; i <= total; i++) {
         if (eval('question' + i) == null || eval('question' + i) == '') {
-            empty++
+            empty++;
             // console.log('you got empty')
         } else if (eval('question' + i) == answers[i - 1]) {
-            score++
+            score++;
             // console.log('you got correct')
         } else {
             wrong++;
             //  console.log('you got wrong')
         }
     }
-    var parsedUrl = new URL(window.location.href);
-    console.log(parsedUrl.searchParams.get("wrong", "score")); // 123
 
     // score = encodeURIComponent(score);
     // wrong = encodeURIComponent(wrong);
     // empty = encodeURIComponent(empty);
-    // var string_url = '/result.html/?score=' + score + '&wrong=' + wrong + '&empty=' + empty;
 
-    // window.location.href = string_url;
-
+    window.location.href = '/result.html?score=' + score + '&wrong=' + wrong + '&empty=' + empty;
 
 }
